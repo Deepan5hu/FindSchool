679e605c31e7be0405271a911654ea727e8a816c
Deepanshu Singh <119125352+Deepan5hu@users.noreply.github.com>
2024-07-20 20:02:11 +0530

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,27 +1,21 @@
-var mysql = require('mysql');
+var mysql   = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
   host     : process.env.Host,
   user     : process.env.User,
   password : process.env.Password,
-  database : process.env.Database,
-  connectTimeout: 10000, // 10 seconds
-  acquireTimeout: 10000, // 10 seconds
-  timeout: 10000, // 10 seconds
-  debug: true
+  database : process.env.Database
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    console.error('Host:', process.env.Host);
-    console.error('User:', process.env.User);
-    console.error('Database:', process.env.Database);
-    return;
-  }
- 
-  console.log('database connection established');
-});
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('database connection established');
+  });
 
-module.exports = { connection };
+module.exports = {connection}
+ 
